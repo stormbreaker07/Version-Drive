e7255c8ba0827f8de2020843b6f146aaf2e04d5a
stormbreaker07 <tanuj07yadav07@gmail.com>
2021-06-22 11:37:19 +0530

    first version 1.00

---
+++ Version Drive/VersionDriver/src/main/java/com/example/versionDriver/VersionDriverApplication.java
@@ -0,0 +1,13 @@
+package com.example.versionDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionDriverApplication.class, args);
+	}
+
+}
---
+++ Version Drive/VersionDriver/src/test/java/com/example/versionDriver/VersionDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versionDriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
